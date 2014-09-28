990e4f978cf87e1cdfcf14082286841880bae3e4
josiahayres <josiahayres@gmail.com>
2014-09-29 09:52:46 +1300

    Update Receptionist.java
    added code to add new customer, just need to have JDBC ODBC in the eclipse project build path

--- src/User/Receptionist.java
+++ src/User/Receptionist.java
@@ -68,6 +68,43 @@ public class Receptionist extends User {
 		input_DOB,
 		input_Phone,
 		input_Balance */
+		
+		
+		try{
+			//init driver to access the database:
+			//creates new statement object that is run as an SQL query:
+			//creates object Statement 'connectDriver' which will generate session with database, and allows for commands to be given to database, and responses recieved
+			//login_DSN is data source connecting to the database, created in setup ODBC data sources  
+			//insert statement. Can use normal SQL queries withing quotes in executeUpdate("SQL statment");
+			//the false true for staff, admin or true = 1, receptionist is false = 0
+			//commit runs all previous lines as one block, not runs one line then the next
+			
+			//Sets up connection with database
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			Connection connectDriver = DriverManager.getConnection("jdbc:odbc:Archive_DSN");
+			Statement insertStatement = connectDriver.createStatement();
+			
+			//SQL querys:
+			
+			
+				
+			
+			insertStatement.executeUpdate("INSERT INTO Customer ('" + inputID + "', '" 
+					+ input_FirstName + "', '"
+					+ input_LastName + "', '"
+					+ input_Address + "', '"
+					+ input_Email + "', '"
+					+ input_DOB + "', '"
+					+ input_Phone + "', '"
+					+ input_Balance + "', '"
+					+ input_Password + "');");
+			
+			
+			connectDriver.commit();
+			
+		}catch(Exception e){
+			System.err.println(e);
+		}
 	}
 	
 	public void updateCustomerInfo(){
